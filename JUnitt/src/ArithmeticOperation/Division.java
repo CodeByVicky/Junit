7dfe06c2852efffbb2fae2d48ddb9027ae5fa730
vicky <shree@DESKTOP-B61L2OT>
2023-04-21 22:14:29 +0530

    Basic Program

---
+++ JUnitt/src/ArithmeticOperation/Division.java
@@ -0,0 +1,12 @@
+package ArithmeticOperation;
+
+import org.junit.Test;
+
+public class Division {
+
+	@Test
+	public int add(int a, int b) {
+		return a / b;
+	}
+
+}
---
+++ JUnitt/src/ArithmeticOperation/Multification.java
@@ -0,0 +1,12 @@
+package ArithmeticOperation;
+
+import org.junit.Test;
+
+public class Multification {
+
+	@Test
+	public int add(int a, int b) {
+		return a * b;
+	}
+
+}
